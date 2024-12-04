1b5825699ab4040ddc3ae2579fca073753edd15f
EricPR1 <epetterssonruiz@gmail.com>
2024-12-04 06:44:45 -0500

    Lab finished

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
+''''Detects emotions by calling emotion_Detection.py'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
-def emDetector():
+def em_detector():
+    ''' Detects the emotion of the sentence '''
     emotion_text = request.args.get('textToAnalyze')
 
     response = emotion_detector(emotion_text)
+    if response['anger'] is None:
+        return "Invalid text! Please try again!"
 
     resp_return = 'For the given statement, the system response is '
     for key, value in response.items():
@@ -24,6 +28,7 @@ def emDetector():
 
 @app.route("/")
 def render_index_page():
+    '''renders the template index'''
     return render_template('index.html')
 
 if __name__ == "__main__":
